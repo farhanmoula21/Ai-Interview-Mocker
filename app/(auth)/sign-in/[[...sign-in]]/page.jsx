423cf5dbceddaef8cd808576139fa19fc5db2dce
Mohd Salah Shakeel Shaikh <salahshaikh1836@gmail.com>
2025-04-16 18:35:39 +0530

    Initial Commit

--- app/(auth)/sign-in/[[...sign-in]]/page.jsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.jsx
@@ -6,8 +6,8 @@ export default function Page() {
       <div className="lg:grid lg:min-h-screen lg:grid-cols-12">
         <section className="relative flex h-32 items-end bg-gray-900 lg:col-span-5 lg:h-full xl:col-span-6">
           <img
-            alt=""
-            src="https://www.southmoorschool.co.uk/wp-content/uploads/job_interview_illustration.jpg"
+            alt="Job Interview"
+            src="/job-interview.jpg"
             className="absolute inset-0 h-full w-full object-cover opacity-80"
           />
 
