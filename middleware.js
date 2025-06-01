ed121ed1640ce6acd4298ff1c73a3e0d0e847bf5
FARHAN MOULA <149301548+farhanmoula21@users.noreply.github.com>
2025-06-01 21:47:17 +0530

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -1,18 +1,14 @@
 import { auth, clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
+
 const isProtectedRoute = createRouteMatcher([
-  '/dashboard(.*)',
-  '/forum(.*)'
-]);
+    '/dashboard(.*)',
+    '/forum(.*)'
+])
 
-export default clerkMiddleware((auth, req) => {
-  if (isProtectedRoute(req)) {
-    return auth().protect(); // ✅ must return this
-  }
-  return NextResponse.next(); // ✅ return something for public routes
-});
-
-import { NextResponse } from "next/server";
+export default clerkMiddleware((auth,req)=>{
+    if(isProtectedRoute(req)) auth().protect();
+})
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
